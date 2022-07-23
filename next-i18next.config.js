119f16fa5d7ae2c56c6128f9fe149ffde1a747c4
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-07-23 19:32:23 -0300

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "ptBr"],
+    defaultLocale: 'en',
+    locales: ['en', 'ptBr'],
   },
+  react: { useSuspense: false },
 };
