f14effaf383c604eb5e67b9d702e89cfa39c6531
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-07-23 19:54:59 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   i18n,
   reactStrictMode: true,
   swcMinify: true,
+  target: 'serverless',
   experimental: { nftTracing: true },
   compiler: {
     styledComponents: true,
