458e8e23a9734f8e067f7bc35c2193a769ad70fe
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-08-15 18:35:05 -0300

    Update styles.js

--- src/templates/Auth/styles.js
+++ src/templates/Auth/styles.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.main`
   ${({ theme }) => css`
     background-color: ${theme.colors.fourthColor};
     min-height: 100vh;
